bf77d109002b04fc59139f2da61789af198239c1
fw1002 <103230559+fw1002@users.noreply.github.com>
2025-03-20 11:36:12 +0800

    Update search.js

--- functions/api/search.js
+++ functions/api/search.js
@@ -29,3 +29,16 @@ export async function onRequest(context) {
     headers: { 'Content-Type': 'application/json' }
   });
 }
+const tokenRes = await fetch('https://test.api.amadeus.com/v1/security/oauth2/token', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: `grant_type=client_credentials&client_id=${env.API_KEY}&client_secret=${env.API_SECRET}`
+});
+const tokenData = await tokenRes.json();
+
+// Log 出 token 回傳
+console.log("Token Response:", tokenData);
+
+const token = tokenData.access_token;
